115d039b14806f7c0636de950e89aa48cde2beac
Nuttapong Mekvipad <n.mekvipad@hotmail.com>
2022-07-03 12:46:29 +0200

    Finish MultiHeadEdgeAttention

--- src/model/layers.py
+++ src/model/layers.py
@@ -1,11 +1,22 @@
+import torch
 from torch_geometric.utils import softmax
 from torch_geometric.nn import MessagePassing
 
 
 # message passing for edge aggregation https://github.com/pyg-team/pytorch_geometric/issues/1489
-class EdgeSoftmaxAttention(MessagePassing):
-    def __init__(self):
-        super(EdgeSoftmaxAttention, self).__init__(aggr='add')  # "Add" aggregation.
+class MultiHeadEdgeAttention(MessagePassing):
+    def __init__(self, num_head, in_features):
+        super(MultiHeadEdgeAttention, self).__init__(aggr='add')  # "Add" aggregation.
+        self.num_head = num_head
+        self.in_features = in_features
+
+        # weighted attention take aggregation output concat with node as input
+        self.target_attn = torch.nn.Linear(in_features=self.in_features, out_features=self.num_head)
+        self.metapath_attn = torch.nn.Parameter(torch.empty(size=(self.num_head, self.in_features)))
+
+        # init weight
+        torch.nn.init.xavier_normal_(self.target_attn.weight)
+        torch.nn.init.xavier_normal_(self.metapath_attn.data)
 
     def forward(self, x, edge_index, edge_attr):
         # edge_index is an array of shape (2, E) when E is the number of edges
@@ -17,12 +28,21 @@ class EdgeSoftmaxAttention(MessagePassing):
         # x_i and x_j are automatically generate from x by PyG
         return self.propagate(edge_index, x=x, edge_attr=edge_attr)
 
-    def message(self, x_j, edge_attr, edge_index):
-        # calculate attention weighted feature vector for each edge
-        source, destination = edge_index
-        attn_val = softmax(edge_attr, index=destination)
+    def message(self, x_i, edge_attr, edge_index):
+        # target node attention
+        target_attention = self.target_attn(x_i)  # (n_edge, num_head)
 
-        return attn_val * x_j
+        # metapath attention
+        metapath_attention = edge_attr * self.metapath_attn  # (n_edge, num_head, feat_dim) * (num_head, feat_dim)
+        metapath_attention = metapath_attention.sum(dim=-1)  # (n_edge, num_head)
+
+        # calculate attention weight
+        e_p = target_attention + metapath_attention
+        # (n_edge, softmax(num_head)); softmax is applied along axis 1 for each col
+        attn = softmax(e_p, edge_index.permute(1, 0)[:, -1])
+        attn_weighted_feat = edge_attr * attn.unsqueeze(dim=-1)   # (n_edge, num_head, feat_dim) * (n_edge, num_head, 1)
+
+        return attn_weighted_feat.view(-1, self.num_head * self.in_features)  # (n_edge, num_head * feat_dim)
 
     def update(self, aggr_out):
         # aggr_out has shape [N, out_channels]
--- src/model/nn.py
+++ src/model/nn.py
@@ -1,2 +1,82 @@
-from src.model.layers import EdgeSoftmaxAttention
+import torch
+import torch.nn.functional as F
+from torch_geometric.utils import softmax
+from src.model.layers import MultiHeadEdgeAttention
 
+
+#TODO: currentlt written for single head case, need to expand to multi-head
+class IntraMetaPathAggregation(torch.nn.Module):
+    def __init__(self, hidden_size, num_head=1, aggregator_type='mean'):
+        super().__init__()
+
+        # model hyperparameters
+        self.aggregator_type = aggregator_type
+        self.hidden_size = hidden_size
+        self.num_head = num_head
+
+        # model aggregator
+        if aggregator_type == 'GRU':
+            # single layer GRU
+            # output hidden layer will be reshape into (hidden_size, num_head)
+            self.aggregator = torch.nn.GRU(input_size=hidden_size, hidden_size=hidden_size * num_head, batch_first=True)
+        elif aggregator_type == 'mean':
+            pass
+        else:
+            raise ValueError("aggregator_type can only be values in ['GRU', 'mean']")
+
+        self.attention = MultiHeadEdgeAttention(num_head=num_head, in_features=hidden_size)
+
+    def forward(self, node_features, edge_index, metapath_idx):
+        #TODO:  Node feature and edge pairs to be encompassed in PyG graph class.
+        #       Dim of each layer to be verified and test
+
+        # feature look up for nodes in each metapath
+        metapath_feat = F.embedding(metapath_idx, node_features)  # (n_edge, n_metapath_len, hidden_size)
+
+        # metapath aggregation
+        if self.aggregator_type == 'mean':
+            agg_feat = torch.mean(metapath_feat, dim=1)  # (n_edge, hidden_size)
+            agg_feat = torch.concat([metapath_feat] * self.num_head, dim=-1)  # (n_edge, hidden_size * num_head)
+
+        elif self.aggregator_type == 'GRU':
+            _, agg_feat = self.aggregator(input=metapath_feat)  # (1, n_edge, hidden_size * num_head)
+            agg_feat = agg_feat.squeeze(dim=0)  # (n_edge, hidden_size * num_head)
+
+        # reshape and swap dim
+        agg_feat = agg_feat.view(-1, self.num_head, self.hidden_size)  # (n_edge, num_head, hidden_size)
+
+        # edge attention
+        # (n_edge, num_head * feat_dim)
+        updated_node_feat = self.attention(x=node_features, edge_index=edge_index, edge_attr=agg_feat)
+
+        return updated_node_feat
+
+
+class InterMetaPathAggregation(torch.nn.Module):
+    def __init__(self, hidden_dim):
+        super().__init__()
+
+    def forward(self):
+        pass
+
+
+class MAGNN(torch.nn.Module):
+    def __init__(self, hidden_dim):
+        super().__init__()
+
+    def forward(self):
+        # map node feature to the same dimension
+
+        #####################################
+        #         For each node type        #
+        #####################################
+
+        # intra metapath aggregation
+
+        # inter metapath aggregation
+
+        # message passing
+
+        # update node feature
+
+        pass
